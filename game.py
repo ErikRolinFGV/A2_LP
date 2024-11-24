5bc14a6640dd54cfd787486447092d590384ed3a
BrunoCavalli <brunocavalli12b@gmail.com>
2024-11-24 20:08:44 -0300

    começei o map_editor

--- game.py
+++ game.py
@@ -11,7 +11,7 @@ class Game:
     def __init__(self):
         pygame.init()
 
-        pygame.display.set_caption('ninja game')
+        pygame.display.set_caption('??')
         self.screen = pygame.display.set_mode((640, 480))
         self.display = pygame.Surface((320, 240))
 
